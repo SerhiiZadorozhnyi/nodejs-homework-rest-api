beba567de519f78ad6488c6b7e11838b3ca3c5ef
Serhii Zadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-10-09 00:23:42 +0300

    Clear code

--- models/index.js
+++ models/index.js
@@ -1,11 +1,7 @@
-// const { joiSchema } = require('./contact')
-// const { joiSchemaStatusContact } = require('./contact')
 const { Contact } = require('./contact')
 const { User } = require('./user')
 
 module.exports = {
-  // joiSchema,
-  // joiSchemaStatusContact,
   Contact,
   User
 }
