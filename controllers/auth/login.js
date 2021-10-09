dc8dd0961758692c5754643a10bea5abf98192b6
Serhii <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-10-09 21:42:35 +0300

    Add (Login and Logout) controllers

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,5 +1,30 @@
-const login = async(req, res) => {
+const { BadReques } = require('http-errors')
+// const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+const { User } = require('../../models')
+
+const { SECRET_KEY } = process.env
+
+const login = async(req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email }, '_id email password')
+  if (!user || !user.comparePassword(password)) {
+    throw new BadReques('Invalid email or password')
+  }
+  const { _id } = user
+  const payload = {
+    _id
+  }
+  const token = jwt.sign(payload, SECRET_KEY)
+  await User.findByIdAndUpdate(_id, { token })
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      token
+    }
+  })
 }
 
 module.exports = login
