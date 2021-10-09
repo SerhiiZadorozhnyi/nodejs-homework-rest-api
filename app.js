2f0cc06d643eb1618237d6fa4eae12313db9c971
Serhii <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-10-09 21:29:21 +0300

    Update app.js and full comment server.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 // const logger = require('morgan')
 const cors = require('cors')
-const mongose = require('mongoose')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 const authRouter = require('./routes/api/auth')
+const ordersRouter = require('./routes/api/orders')
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
@@ -17,10 +18,12 @@ app.use(express.json())
 
 app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
+app.use('/api/orders', ordersRouter)
 
 app.use((req, res) => {
   res.status(404).json({
     status: 'error',
+    code: 404,
     message: 'Not found'
   })
 })
@@ -30,4 +33,15 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message })
 })
 
-module.exports = app
+// module.exports = app
+const { DB_HOST, PORT = 3000 } = process.env
+
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => app.listen(PORT))
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1)
+  })
--- controllers/orders/getAllByUser.js
+++ controllers/orders/getAllByUser.js
@@ -2,7 +2,7 @@ const { Order } = require('../../models')
 
 const getAllByUser = async(req, res) => {
   const { _id } = req.user
-  const result = await Order.find({ owner: _id }, '_id content owner');
+  const result = await Order.find({ owner: _id }, '_id content owner')
   res.json({
     status: 'success',
     code: 200,
